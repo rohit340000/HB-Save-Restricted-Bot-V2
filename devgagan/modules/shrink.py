a6c677c81fcf15c6d663376b799eb79f05dbab23
Calingrok <ramesh_mirdha@outlook.com>
2025-03-10 01:22:13 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,7 +73,7 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
+        image_url = "https://telegra.ph/file/cad7038fe82e47f79c609.jpg"
         join_button = InlineKeyboardButton("Join Channel", url="https://t.me/jaat_one")
         premium = InlineKeyboardButton("Get Premium", url="https://t.me/star_jaat_bot")   
         keyboard = InlineKeyboardMarkup([
