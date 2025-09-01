af525d7c5af8d9c19fb34831e70d11fb5f0f59c3
Harish Choudhari <168732974+harishjat77@users.noreply.github.com>
2025-09-01 22:21:56 +0530

    Update stats.py

--- devgagan/modules/stats.py
+++ devgagan/modules/stats.py
@@ -2,10 +2,10 @@
 # File Name: stats.py
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
+# Author: Harish JaaT
+# GitHub: https://github.com/harishjat77/
+# Telegram: https://t.me/HarishXBots
+# YouTube: https://youtube.com/@TalkWithHB
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
 # Version: 2.0.5
